b94b2b56321fe151dcb346a615ec4d8cc65dc534
amirrasooli69 <amirrasooli69@yahoo.com>
2024-12-25 15:42:27 +0330

    declear request for doctor

--- src/modules/clinic/clinic.service.ts
+++ src/modules/clinic/clinic.service.ts
@@ -1,25 +1,48 @@
-import { BadRequestException, ConflictException, ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  Scope,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ClinicEntity } from "./entity/clinic.entity";
-import { FindOptionsWhere, ILike, LessThanOrEqual, MoreThanOrEqual, Repository } from "typeorm";
+import {
+  FindOptionsWhere,
+  ILike,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  Repository,
+} from "typeorm";
 import { ClinicDetailEntity } from "./entity/detail.entity";
 import { ClinicDoctorEntity } from "./entity/doctors.entity";
 import { ClinicDocumentEntity } from "./entity/document.entity";
 import { CreateClinicDto } from "./dto/clinic.dto";
-import { ConfilictMessage, ForbiddenMessage, NotFoundMessage, PublicMessage } from "src/common/enum/message.enum";
+import {
+  ConfilictMessage,
+  ForbiddenMessage,
+  NotFoundMessage,
+  PublicMessage,
+} from "src/common/enum/message.enum";
 import { isDate, isEnum, isMobilePhone, isPhoneNumber } from "class-validator";
 import { CategoryService } from "../category/category.service";
 import { getCityAndProvinceNameByCode } from "src/common/util/city.util";
 import slugify from "slugify";
 import { S3Service } from "../s3/s3.service";
 import { PaginationDto } from "src/common/dto/pagination.dto";
-import { paginationGenerator, paginationSolver } from "src/common/util/pagination.util";
+import {
+  paginationGenerator,
+  paginationSolver,
+} from "src/common/util/pagination.util";
 import { ClinicStatus } from "./enum/status.enum";
 import { ClinicFilterDto } from "./dto/filter.dto";
-import { RejectDto } from "./dto/reject.dto";
 import { CreateDoctorDto } from "./dto/doctor.dto";
+import { REQUEST } from "@nestjs/core";
+import { Request } from "express";
 
-@Injectable()
+@Injectable({scope: Scope.REQUEST})
 export class ClinicService {
   constructor(
     @InjectRepository(ClinicEntity)
@@ -31,7 +54,8 @@ export class ClinicService {
     @InjectRepository(ClinicDocumentEntity)
     private documentRepository: Repository<ClinicDocumentEntity>,
     private categoryService: CategoryService,
-    private s3Service: S3Service
+    private s3Service: S3Service,
+    @Inject(REQUEST) private request: Request
   ) {}
 
   async register(dto: CreateClinicDto, files: Express.Multer.File[]) {
@@ -129,26 +153,29 @@ export class ClinicService {
     const { limit, page, skip } = paginationSolver(paginationDto);
     const { search, status, from_date, to_date } = filterDto;
     let where: FindOptionsWhere<ClinicEntity> = {};
-    if(status && isEnum(status, ClinicStatus)){
-      where['status'] = status;
+    if (status && isEnum(status, ClinicStatus)) {
+      where["status"] = status;
     }
-    if(search && search?.length > 2) {
-      where["name"]= ILike(`%${search}%`)
+    if (search && search?.length > 2) {
+      where["name"] = ILike(`%${search}%`);
     }
-    
-    if(to_date && from_date && isDate(new Date(to_date) && isDate(new Date(from_date)))){
-      let to =new Date( new Date(to_date).setUTCHours(0, 0, 0));
-      let from =new Date( new Date(from_date).setUTCHours(0, 0, 0));
 
-      where["created_at"] =(MoreThanOrEqual(from) ,LessThanOrEqual(to));
-    } else if(from_date && isDate(new Date(from_date))){
-      let from =new Date( new Date(from_date).setUTCHours(0, 0, 0));
+    if (
+      to_date &&
+      from_date &&
+      isDate(new Date(to_date) && isDate(new Date(from_date)))
+    ) {
+      let to = new Date(new Date(to_date).setUTCHours(0, 0, 0));
+      let from = new Date(new Date(from_date).setUTCHours(0, 0, 0));
+
+      where["created_at"] = (MoreThanOrEqual(from), LessThanOrEqual(to));
+    } else if (from_date && isDate(new Date(from_date))) {
+      let from = new Date(new Date(from_date).setUTCHours(0, 0, 0));
       where["created_at"] = MoreThanOrEqual(from);
-    } else if(to_date && isDate(new Date(to_date))){
-      let to =new Date( new Date(to_date).setUTCHours(0, 0, 0));
+    } else if (to_date && isDate(new Date(to_date))) {
+      let to = new Date(new Date(to_date).setUTCHours(0, 0, 0));
       where["created_at"] = LessThanOrEqual(to);
     }
-
 
     const [clinics, count] = await this.clinicRepository.findAndCount({
       where,
@@ -158,63 +185,76 @@ export class ClinicService {
     });
     return {
       pagination: paginationGenerator(page, limit, count),
-      clinics
-    }
+      clinics,
+    };
   }
 
-  async findOne(id: number){
-    const clinic = await this.clinicRepository.findOneBy({id});
-    if(!clinic) throw new NotFoundException(NotFoundMessage.NotFound)
-      return clinic;
+  async findOne(id: number) {
+    const clinic = await this.clinicRepository.findOneBy({ id });
+    if (!clinic) throw new NotFoundException(NotFoundMessage.NotFound);
+    return clinic;
   }
 
-  async accept(id: number){
+  async accept(id: number) {
     const clinic = await this.findOne(id);
-    if(clinic.status == ClinicStatus.Confirmed) throw new BadRequestException("کلینیک قبلا تایید شده ")
+    if (clinic.status == ClinicStatus.Confirmed)
+      throw new BadRequestException("کلینیک قبلا تایید شده ");
     clinic.status = ClinicStatus.Confirmed;
     clinic.accepted_at = new Date();
     clinic.reason = null;
     clinic.rejected_at = null;
     await this.clinicRepository.save(clinic);
     return {
-      message: "کلنیک تایید شد"
-    }
+      message: "کلنیک تایید شد",
+    };
   }
 
-  async reject(id: number, reason: string){
+  async reject(id: number, reason: string) {
     const clinic = await this.findOne(id);
-    if(clinic.status == ClinicStatus.Rejected) throw new BadRequestException("کلینیک قبلا رد شده")
-    if(clinic.status == ClinicStatus.Confirmed) throw new BadRequestException("کلینیک قبلا تایید شده و امکان رد آن نیست")
+    if (clinic.status == ClinicStatus.Rejected)
+      throw new BadRequestException("کلینیک قبلا رد شده");
+    if (clinic.status == ClinicStatus.Confirmed)
+      throw new BadRequestException("کلینیک قبلا تایید شده و امکان رد آن نیست");
     clinic.status = ClinicStatus.Rejected;
-  clinic.reason = reason;
-  clinic.rejected_at = new Date();
-  clinic.accepted_at= null;
-  await this.clinicRepository.save(clinic)
-  return {
-    message: "رد کلینیک انجام شد"
-  }
+    clinic.reason = reason;
+    clinic.rejected_at = new Date();
+    clinic.accepted_at = null;
+    await this.clinicRepository.save(clinic);
+    return {
+      message: "رد کلینیک انجام شد",
+    };
   }
 
-  async findOneById(id: number){
-    const clinic = await this.clinicRepository.findOneBy({id});
-    if(!clinic) throw new  NotFoundException(NotFoundMessage.NotFoundClinic)
-    if(clinic.status !== ClinicStatus.Confirmed) {
-      throw new ForbiddenException(ForbiddenMessage.ClinicRejected)
+  async findOneById(id: number) {
+    const clinic = await this.clinicRepository.findOneBy({ id });
+    if (!clinic) throw new NotFoundException(NotFoundMessage.NotFoundClinic);
+    if (clinic.status !== ClinicStatus.Confirmed) {
+      throw new ForbiddenException(ForbiddenMessage.ClinicRejected);
     }
     return clinic;
   }
 
-  async createDoctor(doctorDto: CreateDoctorDto, image: Express.Multer.File){
-    const {degree, experience, firstname, lastname, majors, medical_code}= doctorDto;
-    const doctor = await this.doctorRepository.findOneBy({medical_code});
-    if(doctor) throw new ConflictException(ConfilictMessage.doctor)
+  async createDoctor(doctorDto: CreateDoctorDto, image: Express.Multer.File) {
+    const { degree, experience, firstname, lastname, majors, medical_code } =
+      doctorDto;
+    const doctor = await this.doctorRepository.findOneBy({ medical_code });
+    if (doctor) throw new ConflictException(ConfilictMessage.doctor);
     const newDoctor = this.doctorRepository.create({
-  degree, experience, firstname, lastname, majors, medical_code})
-  if(image) {
-    const {Location}= await this.s3Service.uploadFile(image, "clinic/doctor")
-  }
-  newDoctor.image = Location;
+      degree,
+      experience,
+      firstname,
+      lastname,
+      majors,
+      medical_code,
+    });
+    if (image) {
+      const { Location } = await this.s3Service.uploadFile(
+        image,
+        "clinic/doctor"
+      );
+    }
+    newDoctor.image = Location.toString();
 
-  await this.doctorRepository.save(newDoctor)
+    await this.doctorRepository.save(newDoctor);
   }
 }
